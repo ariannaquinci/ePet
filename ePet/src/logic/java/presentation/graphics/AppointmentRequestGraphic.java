fc5852109b1bb13a35f5bf37f70249c2e450f677
Arianna Quinci <72978793+ariannaquinci@users.noreply.github.com>
2022-06-13 18:54:30 +0200

    Delete ePet directory

--- ePet/src/logic/java/presentation/graphics/AppointmentRequestGraphic.java
+++
@@ -1,7 +0,0 @@
-package presentation.graphics;
-
-public interface AppointmentRequestGraphic {
-    void acceptAppRequest();
-    void denyAppRequest();
-    void setData(String a, String b, String c, String d);
-}
--- ePet/src/logic/java/presentation/graphics/LoginGraphic.java
+++
@@ -1,9 +0,0 @@
-package presentation.graphics;
-
-import java.io.IOException;
-
-public interface LoginGraphic {
-    void goBack() throws IOException;
-
-    void checkLogin() throws IOException;
-}

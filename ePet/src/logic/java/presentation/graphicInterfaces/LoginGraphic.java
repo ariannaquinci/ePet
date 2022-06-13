89814360aad11affb182f01a782fccff4bc9bf74
Arianna Quinci <72978793+ariannaquinci@users.noreply.github.com>
2022-06-13 13:31:10 +0200

    Delete ePet directory

--- ePet/src/logic/java/presentation/graphicInterfaces/LoginGraphic.java
+++
@@ -1,9 +0,0 @@
-package presentation.graphicInterfaces;
-
-import java.io.IOException;
-
-public interface LoginGraphic {
-    void goBack() throws IOException;
-
-    void checkLogin() throws IOException;
-}
--- ePet/src/logic/java/presentation/graphicInterfaces/SlotItemGraphic.java
+++
@@ -1,8 +0,0 @@
-package presentation.graphicInterfaces;
-
-public interface SlotItemGraphic {
-
-    void delete();
-
-    void setData(String date, String address, String p);
-}
